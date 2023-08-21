a8f6e8a7f27d3766eb42f6e8fc4882210fcbf2dd
xlvxiii <pangestualviansyah@gmail.com>
2023-08-21 19:37:22 +0700

    feat: menambahkan seeder permission & role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,5 +16,89 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         //
+        //buat permission
+        Permission::create(['name' => 'create surat masuk']);
+        Permission::create(['name' => 'read surat masuk']);
+        Permission::create(['name' => 'update surat masuk']);
+        Permission::create(['name' => 'delete surat masuk']);
+
+        Permission::create(['name' => 'create surat keluar']);
+        Permission::create(['name' => 'read surat keluar']);
+        Permission::create(['name' => 'update surat keluar']);
+        Permission::create(['name' => 'delete surat keluar']);
+
+        Permission::create(['name' => 'create disposisi']);
+        Permission::create(['name' => 'read disposisi']);
+        Permission::create(['name' => 'update disposisi']);
+        Permission::create(['name' => 'delete disposisi']);
+
+        Permission::create(['name' => 'create jenis disposisi']);
+        Permission::create(['name' => 'read jenis disposisi']);
+        Permission::create(['name' => 'update jenis disposisi']);
+        Permission::create(['name' => 'delete jenis disposisi']);
+
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'read user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'delete user']);
+
+        //buat role
+        $admin = Role::create(['name' => 'Admin']);
+        $unitPengolah = Role::create(['name' => 'Unit Pengolah']);
+        $pimpinan = Role::create(['name' => 'Pimpinan']);
+        $kabag = Role::create(['name' => 'Kepala Bagian']);
+        $staff = Role::create(['name' => 'Staff']);
+
+        //memberikan permission ke role
+        $admin->givePermissionTo([
+            'create user',
+            'read user',
+            'update user',
+            'delete user',
+
+            'create jenis disposisi',
+            'read jenis disposisi',
+            'update jenis disposisi',
+            'delete jenis disposisi'
+        ]);
+
+        $unitPengolah->givePermissionTo([
+            'create surat masuk',
+            'read surat masuk',
+            'update surat masuk',
+            'delete surat masuk',
+
+            'create surat keluar',
+            'read surat keluar',
+            'update surat keluar',
+            'delete surat keluar'
+        ]);
+
+        $pimpinan->givePermissionTo([
+            'read surat masuk',
+            'create disposisi',
+            'read disposisi',
+            'update disposisi',
+            'read jenis disposisi',
+
+            'read user'
+        ]);
+
+        $kabag->givePermissionTo([
+            'read surat masuk',
+            'read disposisi',
+            'update disposisi',
+            'read jenis disposisi',
+
+            'read user'
+        ]);
+
+        $staff->givePermissionTo([
+            'read surat masuk',
+            'read disposisi',
+            'read jenis disposisi',
+
+            'read user'
+        ]);
     }
 }
